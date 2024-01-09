799faeeddcd008bb8559b4130a649a750df07df2
Mari Murray <65345823+MurrayMari@users.noreply.github.com>
2024-01-09 13:47:41 +0000

    Create Home.js

---
+++ WeddingWebsite/pages/Home.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <p className="host">
+        Mari Murray and Jack Stillman Cannot wait to Host you all in Spain
+      </p>
+
+      <h1 className="weddingday">Wedding Day</h1>
+      <p className="WittyLine"> September 13th</p>
+      <h2 className="venue">Casa Palacio Guardiola</h2>
+      <h2 className="venue">Puerta de Jerez, 5, Sevilla 41001</h2>
+
+      <h2 className="homesubh">Cermony</h2>
+      <p className="hotelnum"> Time; 3pm</p>
+      <p className="hoteldes">
+        We will be having a short ceremony by our celebrants:
+      </p>
+      <p className="hoteldes"> Kathleen Murray & Anne Stillman</p>
+
+      <h2 className="homesubh">Cocktail Hour</h2>
+      <p className="hotelnum"> Time; 5pm</p>
+      <p className="hoteldes"> Champagne and socialising</p>
+
+      <h2 className="homesubh">Reception</h2>
+      <p className="hotelnum"> Time; 7pm</p>
+      <p className="hoteldes"> Dinner followed by dancing till the wee hours</p>
+
+      <p className="weddingday">Day 2</p>
+      <p className="WittyLine"> September 26th</p>
+      <h2 className="venue">Casa Moreno</h2>
+      <p className="venue">Calle Gamazo, 7, Sevilla 41001</p>
+      <p className="hotelnum"> Time; 4pm</p>
+      <p className="hoteldes">Tapas & Wine in a local spot </p>
+    </div>
+  );
+};
+
+export { Home };
